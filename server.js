1971dd7ee97ad562ee33444b66e6f8f343f8f3f5
Ote Leo <ote.leo73@gmail.com>
2020-12-25 13:33:41 +0200

    Server Setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const hostname = process.env.HOSTNAME || '127.0.0.1';
+const port = process.env.PORT || 5000;
+
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World!\n');
+});
+
+
+server.listen(port, hostname, () => {
+    console.log(`Server is running at http://${hostname}:${port}/`);
+});
