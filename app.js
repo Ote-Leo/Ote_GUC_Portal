e60e11f327e81158e56657f2a1d56d99f79b5a3d
Ote Leo <ote.leo73@gmail.com>
2020-12-25 13:54:45 +0200

    Server app initialization

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require('express');
-var app = express();
+const app = express();
+
+app.use('/', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('WELCOME TO GUC PORTAL BACK-END DEMO');
+});
 
 module.exports.app = app;
--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-const http = require('http');
+require('dotenv').config();
+const { app } = require('./app');
 
 const hostname = process.env.HOSTNAME || '127.0.0.1';
 const port = process.env.PORT || 5000;
 
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!\n');
-});
-
-
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
     console.log(`Server is running at http://${hostname}:${port}/`);
 });
